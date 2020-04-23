a0277ec635b526b4c3c0679a3ff3297f387e50a2
sjie <ligongdamao@163.com>
2020-04-23 16:19:07 +0800

    'listener'

--- src/main/kotlin/cn/yusite/example/spring/security/web/listener/CustomAuthenticationListener.kt
+++ src/main/kotlin/cn/yusite/example/spring/security/web/listener/CustomAuthenticationListener.kt
@@ -1,17 +1,36 @@
 package cn.yusite.example.spring.security.web.listener
 
 import org.springframework.context.ApplicationListener
+import org.springframework.context.event.EventListener
+import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent
 import org.springframework.security.authentication.event.AuthenticationSuccessEvent
+import org.springframework.security.web.authentication.AuthenticationFailureHandler
 import org.springframework.stereotype.Component
 
 /**
  * 授权成功监听器
  */
-@Component
-class CustomAuthenticationListener: ApplicationListener<AuthenticationSuccessEvent> {
+//@Component
+//class CustomAuthenticationListener: ApplicationListener<AuthenticationSuccessEvent> {
+//
+//    override fun onApplicationEvent(event: AuthenticationSuccessEvent) {
+//
+//        println("授权成功")
+//    }
+//}
 
-    override fun onApplicationEvent(event: AuthenticationSuccessEvent) {
+@Component
+class CustomAuthenticationListener {
+
+    @EventListener
+    fun onApplicationEvent(event: AuthenticationSuccessEvent) {
 
         println("授权成功")
+    }
+
+    @EventListener
+    fun onApplicationEvent(event: AuthenticationFailureBadCredentialsEvent) {
+
+        println("凭证失败")
     }
 }
